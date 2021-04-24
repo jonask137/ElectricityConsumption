3511c68bda7bda426d357d6ef5cb2031d421e6a3
jonask137 <73840082+jonask137@users.noreply.github.com>
2021-04-25 00:13:35 +0200

    Initiating project, creating function for calling the aggregated data

---
+++ BarryApp.R
@@ -0,0 +1,79 @@
+
+library(httr)
+library(dplyr)
+library(jsonlite)
+
+## Getting spot prices
+
+url <-  'https://jsonrpc.barry.energy/json-rpc#Get%20Spot%20Price'
+key <- '/g1Ox71OYwlpyzdaq7+MXkVYA+/wfrOOLdzOxNWIkiTDd7Dp4O6djPmx/qUISwvzKwiD9nt9t08nr2687MJ8mQFgE/bxV8eH2tNU8B0apbpqEBcBcPvlNAd2wWqhdkDJ8SJC/TdIYhsE+tRnvMMKHv0wE5awKc+dG7DQgD9nl1g='
+ContentType <- 'application/json'
+
+body <- '{
+   "method": "co.getbarry.api.v1.OpenApiController.getPrice",
+    "id": 0,
+    "jsonrpc": "2.0",
+    "params": [
+     "DK_NORDPOOL_SPOT_DK1",
+      "2021-04-23T01:00:00Z",
+      "2021-04-23T02:00:00Z"
+    ]
+}'
+
+post <- POST(url = url
+             ,add_headers('Authorization' = paste("Bearer ",key)
+                          ,'Content-Type' = ContentType)
+             ,body = body)
+
+
+content(post,"text") %>% fromJSON() -> data
+View(data[["result"]])
+
+
+## Retrieving metering points - This is some meta data
+
+url <-  'https://jsonrpc.barry.energy/json-rpc#Get%20MeteringPoints'
+key <- '/g1Ox71OYwlpyzdaq7+MXkVYA+/wfrOOLdzOxNWIkiTDd7Dp4O6djPmx/qUISwvzKwiD9nt9t08nr2687MJ8mQFgE/bxV8eH2tNU8B0apbpqEBcBcPvlNAd2wWqhdkDJ8SJC/TdIYhsE+tRnvMMKHv0wE5awKc+dG7DQgD9nl1g='
+ContentType <- 'application/json'
+
+body <- '{
+   "method": "co.getbarry.api.v1.OpenApiController.getMeteringPoints",
+    "id": 0,
+    "jsonrpc": "2.0",
+    "params": []
+}'
+
+post <- POST(url = url
+             ,add_headers('Authorization' = paste("Bearer ",key)
+                          ,'Content-Type' = ContentType)
+             ,body = body)
+
+
+content(post,"text") %>% fromJSON() -> data
+
+
+
+## Get Aggregated Consumption
+
+url <-  'https://jsonrpc.barry.energy/json-rpc#Get%20Aggeregated%20Consumption'
+key <- '/g1Ox71OYwlpyzdaq7+MXkVYA+/wfrOOLdzOxNWIkiTDd7Dp4O6djPmx/qUISwvzKwiD9nt9t08nr2687MJ8mQFgE/bxV8eH2tNU8B0apbpqEBcBcPvlNAd2wWqhdkDJ8SJC/TdIYhsE+tRnvMMKHv0wE5awKc+dG7DQgD9nl1g='
+ContentType <- 'application/json'
+
+body <- '{
+   "method": "co.getbarry.api.v1.OpenApiController.getAggregatedConsumption",
+    "id": 0,
+    "jsonrpc": "2.0",
+    "params": [
+    "2021-04-01T01:00:00Z",
+    "2021-04-25T02:00:00Z"
+    ]
+}'
+
+post <- POST(url = url
+             ,add_headers('Authorization' = paste("Bearer ",key)
+                          ,'Content-Type' = ContentType)
+             ,body = body)
+
+
+content(post,"text") %>% fromJSON() -> data
+View(data[["result"]])
---
+++ Extract_Data/Extract_Aggregated_Consumption.R
@@ -0,0 +1,36 @@
+
+
+load_aggr_consumption <- function(key
+                                  ,start_date = "2021-04-01"
+                                  ,end_date){
+
+### Loading libraries
+
+if (!require(httr)) install.packages('httr') 
+library(httr)
+if (!require(dplyr)) install.packages('dplyr') 
+library(dplyr)
+if (!require(jsonlite)) install.packages('jsonlite') 
+library(jsonlite)
+
+
+url <-  'https://jsonrpc.barry.energy/json-rpc#Get%20Aggeregated%20Consumption'
+ContentType <- 'application/json'
+
+body <- paste0('{
+   "method": "co.getbarry.api.v1.OpenApiController.getAggregatedConsumption",
+    "id": 0,
+    "jsonrpc": "2.0",
+    "params\": [\n    \"',start_date,'T01:00:00Z\",\n    \"',end_date,'T02:00:00Z\"\n    ]\n}'
+   )
+
+
+post <- POST(url = url
+             ,add_headers('Authorization' = paste("Bearer ",key)
+                          ,'Content-Type' = ContentType)
+             ,body = body)
+
+
+data <- content(post,"text") %>% fromJSON()
+assign(x = "data",value = data,envir = .GlobalEnv)
+}
