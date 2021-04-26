613fe02b6328274685c8cbcfeef996647262faf5
Jonas Ringive Korsholm <Jonas.k345@gmail.com>
2021-04-26 21:54:02 +0200

    Updating structure

--- Main.R
+++ Main.R
@@ -2,8 +2,7 @@
 library(dplyr)
 
 source("Extract_Data/Extract_Aggregated_Consumption.R")
-
-key <- '/g1Ox71OYwlpyzdaq7+MXkVYA+/wfrOOLdzOxNWIkiTDd7Dp4O6djPmx/qUISwvzKwiD9nt9t08nr2687MJ8mQFgE/bxV8eH2tNU8B0apbpqEBcBcPvlNAd2wWqhdkDJ8SJC/TdIYhsE+tRnvMMKHv0wE5awKc+dG7DQgD9nl1g='
+source(".envr.R")
 
 start_date <- "2021-04-01" #YMD
 end_date <- "2021-04-25" #YMD
@@ -11,9 +10,5 @@ end_date <- "2021-04-25" #YMD
 load_aggr_consumption(key = key
                       ,start_date = start_date
                       ,end_date = end_date)
-
-class(data)
-
-summary(data)
 
 data$result %>% head()
